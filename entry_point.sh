582ac7fe881ad597528ba3f6ce0885d8b01885f0
EC2 Default User <ec2-user@ip-172-31-26-255.ec2.internal>
2021-10-24 12:11:50 +0000

    #7: implemented RETAIN_TASK which is disabled by default

--- entry_point.sh
+++ entry_point.sh
@@ -6,8 +6,44 @@ APPIUM_LOG="${APPIUM_LOG:-/var/log/appium.log}"
 CMD="xvfb-run appium --log $APPIUM_LOG"
 
 upload() {
-  echo "TODO: verify if we need extra upload on container exit!"
-#  stop_screen_recording $1 $2
+  echo "Uploading artifacts on container SIGTERM"
+  sessionId=`cat ${APPIUM_LOG} | grep "Session created with session id" | cut -d ":" -f 5`
+  export sessionId=$(echo $sessionId)
+  echo "sessionId inited: $sessionId"
+
+  stop_screen_recording $sessionId ""
+}
+
+retain_appium() {
+  index=$1
+  #be able to handle next session request using existing appium and device/emulator
+
+  sessionId=
+  while [ -z $sessionId ]; do
+    sleep 0.1
+    # 2021-10-22 14:34:46:878 [BaseDriver] Session created with session id: d11cbf4a-c269-4d0e-bc25-37cb93616781
+    sessionId=`cat ${APPIUM_LOG} | grep "Session created with session id" | cut -d ":" -f 5`
+  done
+  export sessionId=$(echo $sessionId)
+  echo "sessionId inited: $sessionId"
+
+  start_screen_recording
+
+  #TODO: mode driver quit/close into the embeded function
+  isExited=
+  while [ -z $isExited ]; do
+    sleep 0.1
+    #2021-10-22 16:00:21:124 [BaseDriver] Event 'quitSessionFinished' logged at 1634918421124 (09:00:21 GMT-0700 (Pacific Daylight Time))
+    isExited=`cat ${APPIUM_LOG} | grep quitSessionFinished | cut -d "'" -f 2`
+#    echo isExited: $isExited
+  done
+
+  restart_appium $index
+  echo "session $sessionId finished. Collecting video and log artifacts..."
+  stop_screen_recording $sessionId $index
+
+  #reset sessionId
+  sessionId=
 }
 
 restart_appium() {
@@ -15,7 +51,7 @@ restart_appium() {
 
   # kill and restart appium & xvfb-run asap to be ready for the next session
   pkill -x node
-  mv "${APPIUM_LOG}" "${APPIUM_LOG}.$index"
+  mv "${APPIUM_LOG}" "${APPIUM_LOG}$index"
   pkill -x xvfb-run
   pkill -x Xvfb
   rm -rf /tmp/.X99-lock
@@ -28,14 +64,16 @@ restart_appium() {
 }
 
 start_screen_recording() {
-  sessionId=$1
-  if [ ! -z $BUCKET ] && [ ! -z $TENANT ] && [ ! -z $sessionId ]; then
-   #TODO: wait until application or browser started otherwise 5-10 sec of Appium Settings app is recorded as well. Limit waiting by 10 seconds and start with recording anyway!
-   # potential line to track valid session startup: "Screen already unlocked, doing nothing"
-    echo "Start video recording for $sessionId"
+  if [ ! -z $BUCKET ] && [ ! -z $TENANT ]; then
+    #forcibly kill any existing screenrecord process to avoid recordings of previous sessions
+    adb shell "su root pkill -f screenrecord"
+
+    #TODO: wait until application or browser started otherwise 5-10 sec of Appium Settings app is recorded as well. Limit waiting by 10 seconds and start with recording anyway!
+    # potential line to track valid session startup: "Screen already unlocked, doing nothing"
+    echo "Starting video recording..."
     adb shell "su root screenrecord /video.mp4" &
-  else 
-    echo "No sense to start screen recording as sessionId not detected!"
+  else
+    echo "No sense to start screen recording as integration with S3 storage not available!"
   fi
 }
 
@@ -49,17 +87,17 @@ stop_screen_recording() {
     adb shell "su root pkill -l 2 -f screenrecord"
     sleep 1
     adb shell "su root chmod a+r video.mp4"
-    adb pull /video.mp4 "video_$index.mp4"
+    adb pull /video.mp4 "video$index.mp4"
 
     #upload session artifacts
     S3_KEY_PATTERN=s3://${BUCKET}/${TENANT}/artifacts/test-sessions/${sessionId}
     echo S3_KEY_PATTERN: ${S3_KEY_PATTERN}
 
     date
-    aws s3 cp "${APPIUM_LOG}.$index" "${S3_KEY_PATTERN}/session.log"
-    aws s3 cp "video_$index.mp4" "${S3_KEY_PATTERN}/video.mp4"
-    rm -f "${APPIUM_LOG}.$index"
-    rm -f "video_$index.mp4"
+    aws s3 cp "${APPIUM_LOG}$index" "${S3_KEY_PATTERN}/session.log"
+    aws s3 cp "video$index.mp4" "${S3_KEY_PATTERN}/video.mp4"
+    rm -f "${APPIUM_LOG}$index"
+    rm -f "video$index.mp4"
     date
   else
     echo "No sense to stop screen recording as sessionId not detected!"
@@ -110,43 +148,21 @@ fi
 pkill -x xvfb-run
 rm -rf /tmp/.X99-lock
 
-trap 'upload $sessionId' SIGTERM
+trap 'upload' SIGTERM
 
 $CMD &
 
-declare -i index=0
-while true; do
-  echo "starting session $i supervisor..."
-  #TODO: move sessionId detection into the embedded function
-  sessionId=
-  while [ -z $sessionId ]; do
-    sleep 0.1
-    # 2021-10-22 14:34:46:878 [BaseDriver] Session created with session id: d11cbf4a-c269-4d0e-bc25-37cb93616781
-    sessionId=`cat ${APPIUM_LOG} | grep "Session created with session id" | cut -d ":" -f 5`
+if [ "$RETAIN_TASK" = true ]; then
+  declare -i index=0
+  while true; do
+    echo "starting session $i supervisor..."
+    retain_appium $index
+    index+=1
+    echo "finished session $i supervisor."
   done
-  export sessionId=$(echo $sessionId)
-  echo "sessionId inited: $sessionId"
-
-  start_screen_recording $sessionId
-
-  #TODO: mode driver quit/close into the embeded function
-  isExited=
-  while [ -z $isExited ]; do
-    sleep 0.1
-    #2021-10-22 16:00:21:124 [BaseDriver] Event 'quitSessionFinished' logged at 1634918421124 (09:00:21 GMT-0700 (Pacific Daylight Time))
-    isExited=`cat ${APPIUM_LOG} | grep quitSessionFinished | cut -d "'" -f 2`
-#    echo isExited: $isExited
-  done
-
-  restart_appium $index
-  echo "session $sessionId finished. Collecting video and log artifacts..."
-  stop_screen_recording $sessionId $index
-
-  #reset sessionId
-  sessionId=
-  index+=1
-  echo "finished session $i supervisor."
-done
+else
+  start_screen_recording
+fi
 
 echo "waiting until SIGTERM received"
 while true; do :; done
