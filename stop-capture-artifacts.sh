e9cf626914a608071c2d177010c6e0aa32800ac0
EC2 Default User <ec2-user@ip-172-31-26-255.ec2.internal>
2021-11-22 22:51:45 +0000

    made screenrecord stop emulator/device compatible

--- stop-capture-artifacts.sh
+++ stop-capture-artifacts.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+pkill -f screenrecord
+
 #kill screenrecord on emulator/device
-adb shell "pkill -l 2 -f screenrecord"
+#adb shell "pkill -l 2 -f screenrecord"
 # sleep was required to finish kill process correctly so video file is closed and editable/visible later.
 # as of now `sleep 1` moved onto the entry_point.sh to be controlled on high level, also testing sleep 0.5
 #sleep 1
 
 #kill capture-artifacts.sh parent shell script
-pkill -f capture-artifacts.sh
+#pkill -f capture-artifacts.sh
