d663325f71c7af56ca00e46cf151c004dbb68397
vdelendik <vdelendik@solvd.com>
2021-12-28 20:30:27 +0300

    improved WDA settings reader

--- files/ios.sh
+++ files/ios.sh
@@ -5,13 +5,23 @@ for ((i=1; i<=$WDA_WAIT_TIMEOUT; i++))
 do
  if [ -f ${WDA_ENV} ]
   then
-   source ${WDA_ENV}
+   cat ${WDA_ENV}
    break
   else
-   echo "Waiting until WDA starts $i sec"
+   echo "Waiting until WDA settings appear $i sec"
    sleep 1
  fi
 done
+
+if [ ! -f ${WDA_ENV} ]; then
+  echo "ERROR! Unable to get WDA settings from STF!"
+  exit -1
+fi
+
+#source wda.env file
+source ${WDA_ENV}
+. ${WDA_ENV}
+export
 
 export AUTOMATION_NAME='XCUITest'
 #TODO: move DEVICETYPE into the WDA_ENV
