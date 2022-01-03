73e924b1204227bfdd317058b0d09711e6ca2dd7
vdelendik <vdelendik@solvd.com>
2022-01-03 20:08:25 +0300

    #30: patched ios-deploy and utils.js to be able to start ios app by bundleId

--- files/downloader/appium-patch.sh
+++ files/downloader/appium-patch.sh
@@ -6,3 +6,12 @@ cp /opt/downloader/appium-base-driver/lib/basedriver/mcloud-utils.js ${APPIUM_HO
 cp ${APPIUM_HOME}/node_modules/appium-base-driver/build/lib/basedriver/helpers.js ${APPIUM_HOME}/node_modules/appium-base-driver/build/lib/basedriver/helpers.js.bak
 cp /opt/downloader/appium-base-driver/build/lib/basedriver/helpers.js ${APPIUM_HOME}/node_modules/appium-base-driver/build/lib/basedriver/helpers.js
 cp /opt/downloader/appium-base-driver/build/lib/basedriver/mcloud-utils.js ${APPIUM_HOME}/node_modules/appium-base-driver/build/lib/basedriver/mcloud-utils.js
+
+
+# wda build/lib files
+cp ${APPIUM_HOME}/node_modules/appium-xcuitest-driver/build/lib/ios-deploy.js ${APPIUM_HOME}/node_modules/appium-xcuitest-driver/build/lib/ios-deploy.js.bak
+cp /opt/downloader/appium-xcuitest-driver/build/lib/ios-deploy.js ${APPIUM_HOME}/node_modules/appium-xcuitest-driver/build/lib/ios-deploy.js
+
+cp ${APPIUM_HOME}/node_modules/appium-xcuitest-driver/build/lib/utils.js ${APPIUM_HOME}/node_modules/appium-xcuitest-driver/build/lib/utils.js.bak
+cp /opt/downloader/appium-xcuitest-driver/build/lib/utils.js ${APPIUM_HOME}/node_modules/appium-xcuitest-driver/build/lib/utils.js
+
