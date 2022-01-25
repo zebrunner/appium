42ff56a966500880f0966d82e970d0920b9ea159
vdelendik <vdelendik@solvd.com>
2022-01-25 14:10:51 +0300

    made apium-patch echoing into the log actual actions

--- files/mcloud/appium-patch.sh
+++ files/mcloud/appium-patch.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+
 #For local usage on Mac:
 #export APPIUM_HOME=/opt/homebrew/lib/node_modules/appium
 #cp -R ./files/mcloud/* ${APPIUM_HOME}/node_modules
 
-cp --recursive --backup=numbered /opt/mcloud ${APPIUM_HOME}/node_modules
+cp -r -v --backup=numbered /opt/mcloud ${APPIUM_HOME}/node_modules
