5a95155693e4840beea34649e5b0cd0f718cc64e
vdelendik <vdelendik@solvd.com>
2022-12-13 02:18:02 +0300

    #146: register WDA_CRASHED=true asap

--- files/check-wda.sh
+++ files/check-wda.sh
@@ -24,6 +24,9 @@ do
   nc ${WDA_HOST} ${MJPEG_PORT}
   echo "netcat connection is closed."
 
+  #146 egister WDA_CRASHED=true env var inside WDA_ENV file 
+  echo "export WDA_CRASHED=true" >> ${WDA_ENV}
+
   # as only connection corrupted start wda again
   /opt/start-wda.sh
 done
