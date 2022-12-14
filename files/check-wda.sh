a844cad4c27b22f310a515ba0f5bcd4bcd0adac2
vdelendik <vdelendik@solvd.com>
2022-12-14 17:52:58 +0300

    add support of iOS devices without wi-fi connection

--- files/check-wda.sh
+++ files/check-wda.sh
@@ -5,21 +5,6 @@ while true
 do
   echo ""
 
-  if [ -f ${WDA_ENV} ] && [ -s ${WDA_ENV} ]; then
-    # source only if exists and non-empty
-    source ${WDA_ENV}
-  else
-    echo "waiting for valid ${WDA_ENV} file content"
-    sleep 1
-    continue
-  fi
-
-  if [ "${WDA_HOST}" == "localhost" ]; then
-    echo "ERROR! WDA started using localhost! Verify device Wi-Fi connection!"
-    echo "Sleeping ${UNREGISTER_IF_STILL_DOWN_AFTER}ms..."
-    sleep $((UNREGISTER_IF_STILL_DOWN_AFTER/1000))
-  fi
-
   echo "Connecting to ${WDA_HOST} ${MJPEG_PORT} using netcat..."
   nc ${WDA_HOST} ${MJPEG_PORT}
   echo "netcat connection is closed."
