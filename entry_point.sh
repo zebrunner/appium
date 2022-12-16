e30b6584aaa43c0c2666a83ebeabd63fcfb378ff
vdelendik <vdelendik@solvd.com>
2022-12-16 15:49:52 +0300

    reuse customized APPIUM_PORT to support distributed setup

--- entry_point.sh
+++ entry_point.sh
@@ -4,7 +4,7 @@ NODE_CONFIG_JSON="/root/nodeconfig.json"
 DEFAULT_CAPABILITIES_JSON="/root/defaultcapabilities.json"
 APPIUM_LOG="${APPIUM_LOG:-/var/log/appium.log}"
 
-CMD="xvfb-run appium --log-no-colors --log-timestamp --log $APPIUM_LOG $APPIUM_CLI"
+CMD="xvfb-run appium --log-no-colors --log-timestamp --port $APPIUM_PORT --log $APPIUM_LOG $APPIUM_CLI"
 
 upload() {
   /opt/stop-capture-artifacts.sh
