15a509b698590d97c1d141ae3a9b6afbad9caa81
vdelendik <vdelendik@solvd.com>
2023-11-07 20:33:02 +0300

    #287: handled cookie DELETE request for video recording correctly

--- entrypoint.sh
+++ entrypoint.sh
@@ -241,7 +241,10 @@ capture_video() {
       #2023-08-08 09:42:51:896 - [HTTP] [HTTP] --> DELETE /wd/hub/session/61e32667-a8f7-4b08-bca7-5092cbccc383
       # or
       #2023-07-20 19:29:56:534 - [HTTP] [HTTP] <-- DELETE /wd/hub/session/3682ea1d-be66-49ad-af0d-792fc3f7e91a 200 1053 ms - 14
-      finishedSessionId=`grep -E -m 1 " DELETE /wd/hub/session/$startedSessionId" ${TASK_LOG} | cut -d "/" -f 5 | cut -d " " -f 1`
+      # make sure to skip cookie DELETE call adding space after startedSessionId value!
+      # 2023-11-02 02:33:46:996 [HTTP] [HTTP] --> DELETE /wd/hub/session/3682ea1d-be66-49ad-af0d-792fc3f7e91a/cookie/id-experiences
+
+      finishedSessionId=`grep -E -m 1 " DELETE /wd/hub/session/$startedSessionId " ${TASK_LOG} | cut -d "/" -f 5 | cut -d " " -f 1`
       #echo "finishedSessionId: $finishedSessionId"
 
       if [ ! -z $finishedSessionId ]; then
