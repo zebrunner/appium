e0d713d5fce5d88cbafd7c0b5418a7dcb608853c
azarouski <azarouski@solvd.com>
2024-04-22 14:56:57 +0200

    Detection of device type and platform version was added

--- files/ios.sh
+++ files/ios.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo DEVICE_UDID: "$DEVICE_UDID"
+
+
+#### Establish usbmuxd communication
 if [[ -z $USBMUXD_SOCKET_ADDRESS ]]; then
   logger "Start containerized usbmuxd service/process"
   usbmuxd -f &
@@ -10,4 +14,20 @@ else
   # rm /var/run/usbmuxd in advance to be able to start socat and join it to $USBMUXD_SOCKET_ADDRESS
   rm -f /var/run/usbmuxd
   socat UNIX-LISTEN:/var/run/usbmuxd,fork,reuseaddr,mode=777 TCP:"$USBMUXD_SOCKET_ADDRESS" &
+fi
+
+
+#### Detect device type and platform version
+deviceInfo=$(curl -s http://${WDA_HOST}:${WDA_PORT})
+
+PLATFORM_VERSION=$(echo "$deviceInfo" | jq -r '.value.os.version')
+export PLATFORM_VERSION
+
+deviceClass=$(echo "$deviceInfo" | jq -r '.value.device')
+export DEVICETYPE='Phone'
+if [ "$deviceClass" = "ipad" ]; then
+  export DEVICETYPE='Tablet'
+fi
+if [ "$deviceClass" = "appletv" ]; then
+  export DEVICETYPE='tvOS'
 fi
