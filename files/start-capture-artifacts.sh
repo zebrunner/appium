24d2bb7e11aac09c16c07198b6cabe8f94da8cdc
azarouski <azarouski@solvd.com>
2024-04-30 13:42:51 +0200

    - start-capture-artifacts.sh was prettified - ffmpeg logs redirection was turned back

--- files/start-capture-artifacts.sh
+++ files/start-capture-artifacts.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
 
-#export
-
 sessionId=$1
 if [ -z $sessionId ]; then
   echo "[warn] [CaptureArtifacts] No sense to record artifacts as sessionId not detected!"
   exit 0
 fi
 
-echo sessionId:$sessionId
-
-echo "[info] [CaptureArtifacts] videoFile: ${sessionId}.mp4"
+echo "[info] [CaptureArtifacts] sessionId: $sessionId"
 
 # send signal to start streaming of the screens from device
-echo "trying to send 'on': nc ${BROADCAST_HOST} ${BROADCAST_PORT}"
+echo "[info] [CaptureArtifacts] trying to send 'on': nc ${BROADCAST_HOST} ${BROADCAST_PORT}"
 echo -n "on" | nc ${BROADCAST_HOST} ${BROADCAST_PORT} -w 0 -v
 
 echo "[info] [CaptureArtifacts] generating video file ${sessionId}.mp4..."
-ffmpeg -v trace -f mjpeg -r 10 -i tcp://${BROADCAST_HOST}:${BROADCAST_PORT} -vf scale="-2:720" -vcodec libx264 -y ${FFMPEG_OPTS} /tmp/${sessionId}.mp4 &
-# > ${VIDEO_LOG} 2>&1
+ffmpeg -v trace -f mjpeg -r 10 -i tcp://${BROADCAST_HOST}:${BROADCAST_PORT} -vf scale="-2:720" -vcodec libx264 -y ${FFMPEG_OPTS} /tmp/${sessionId}.mp4 > ${VIDEO_LOG} 2>&1 &
 
 exit 0
