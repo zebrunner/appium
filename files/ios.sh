7ae8aaa04701890fcb6ac02808bdb1a54cc255bf
vdelendik <vdelendik@solvd.com>
2024-05-17 17:45:53 +0300

    reused USBMUXD_SOCKET_ADDRESS instead of hardcoded 2222 port

--- files/ios.sh
+++ files/ios.sh
@@ -9,7 +9,7 @@ if [[ -z $USBMUXD_SOCKET_ADDRESS ]]; then
   usbmuxd -f &
   sleep 2
   # socat server to share usbmuxd socket via TCP
-  socat TCP-LISTEN:2222,reuseaddr,fork UNIX-CONNECT:/var/run/usbmuxd &
+  socat TCP-LISTEN:${USBMUXD_SOCKET_ADDRESS},reuseaddr,fork UNIX-CONNECT:/var/run/usbmuxd &
 else
   # rm /var/run/usbmuxd in advance to be able to start socat and join it to $USBMUXD_SOCKET_ADDRESS
   rm -f /var/run/usbmuxd
