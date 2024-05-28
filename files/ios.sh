23a47c25fb941864a344310c474b0be748d4c4a1
azarouski <azarouski@solvd.com>
2024-05-28 15:31:30 +0200

    - logger was replaced with echo

--- files/ios.sh
+++ files/ios.sh
@@ -5,13 +5,14 @@ echo DEVICE_UDID: "$DEVICE_UDID"
 
 #### Establish usbmuxd communication
 if [[ -z $USBMUXD_SOCKET_ADDRESS ]]; then
-  logger "Start containerized usbmuxd service/process"
+  echo "Start containerized usbmuxd service/process"
   usbmuxd -f &
   sleep 2
   # socat server to share usbmuxd socket via TCP
   socat TCP-LISTEN:${USBMUXD_SOCKET_ADDRESS},reuseaddr,fork UNIX-CONNECT:/var/run/usbmuxd &
 else
   # rm /var/run/usbmuxd in advance to be able to start socat and join it to $USBMUXD_SOCKET_ADDRESS
+  echo "USBMUXD_SOCKET_ADDRESS was defined as: $USBMUXD_SOCKET_ADDRESS"
   rm -f /var/run/usbmuxd
   socat UNIX-LISTEN:/var/run/usbmuxd,fork,reuseaddr,mode=777 TCP:"$USBMUXD_SOCKET_ADDRESS" &
 fi
