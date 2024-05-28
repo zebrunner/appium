7d7ca18e7c33d9338eb5a51bd3f8b8d8759be8d1
azarouski <azarouski@solvd.com>
2024-05-28 15:59:58 +0200

    - USBMUXD_SOCKET_ADDRESS was replaced to 22 where we establish usbmuxd communication

--- files/ios.sh
+++ files/ios.sh
@@ -9,7 +9,7 @@ if [[ -z $USBMUXD_SOCKET_ADDRESS ]]; then
   usbmuxd -f &
   sleep 2
   # socat server to share usbmuxd socket via TCP
-  socat TCP-LISTEN:${USBMUXD_SOCKET_ADDRESS},reuseaddr,fork UNIX-CONNECT:/var/run/usbmuxd &
+  socat TCP-LISTEN:22,reuseaddr,fork UNIX-CONNECT:/var/run/usbmuxd &
 else
   # rm /var/run/usbmuxd in advance to be able to start socat and join it to $USBMUXD_SOCKET_ADDRESS
   echo "USBMUXD_SOCKET_ADDRESS was defined as: $USBMUXD_SOCKET_ADDRESS"
