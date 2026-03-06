eae33268b2bedc1abf32cabe91d13084a6bbdc33
azarouski <azarouski@solvd.com>
2026-03-06 18:29:59 +0100

    build.sh was added

---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+cmd=(docker build . --progress plain)
+
+tagPath="public.ecr.aws/zebrunner/appium"
+
+if [[ "$1" == "-i" ]]; then
+  shift
+else
+  cmd+=(--target appium)
+fi
+
+if [[ -z "$1" ]]; then
+  echo "Usage: $0 [-i] <tagVersion>"
+  echo "  Where:"
+  echo "    -i          - build docker image with image(photo) processing plugin"
+  echo "     ---"
+  echo "    tagVersion  - desired version for image build (e.g. 3.0, 2.1.5-demo)"
+  exit 1
+else
+  tagVersion=$1
+  cmd+=(--tag "$tagPath":"$tagVersion")
+fi
+
+echo "Following command will be executed:"
+echo ""
+echo "    ${cmd[*]}"
+echo ""
+read -r -p "Press any key to continue or CTRL+C to interrupt execution"
+
+exec "${cmd[@]}"
