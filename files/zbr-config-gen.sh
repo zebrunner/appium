dda73f1b433c8c0997646839e4b29b9bcb808fc2
azarouski <azarouski@solvd.com>
2026-03-09 20:13:12 +0100

    config files indents were fixed

--- files/zbr-config-gen.sh
+++ files/zbr-config-gen.sh
@@ -6,43 +6,43 @@ PLATFORM_NAME=`echo "$PLATFORM_NAME" |  tr '[:upper:]' '[:lower:]'`
 cat << EndOfMessage
 {
   "capabilities":
-      [
-        {
-          "maxInstances": 1,
-          "deviceName": "${DEVICE_NAME}",
-          "deviceType": "${DEVICETYPE}",
-          "platformName":"${PLATFORM_NAME}",
-          "platformVersion":"${PLATFORM_VERSION}",
-	  "udid": "${DEVICE_UDID}",
-	  "adb_port": ${ADB_PORT},
-	  "proxy_port": ${PROXY_PORT},
-          "automationName": "${AUTOMATION_NAME}",
-          "server_proxy_port": ${SERVER_PROXY_PORT}
-        }
-      ],
+    [
+      {
+        "maxInstances": 1,
+        "deviceName": "${DEVICE_NAME}",
+        "deviceType": "${DEVICETYPE}",
+        "platformName":"${PLATFORM_NAME}",
+        "platformVersion":"${PLATFORM_VERSION}",
+        "udid": "${DEVICE_UDID}",
+        "adb_port": ${ADB_PORT},
+        "proxy_port": ${PROXY_PORT},
+        "automationName": "${AUTOMATION_NAME}",
+        "server_proxy_port": ${SERVER_PROXY_PORT}
+      }
+    ],
   "configuration":
-  {
-    "proxy": "com.zebrunner.mcloud.grid.MobileRemoteProxy",
-    "url":"http://${STF_PROVIDER_HOST}:${APPIUM_PORT}/wd/hub",
-    "host": "${STF_PROVIDER_HOST}",
-    "port": ${APPIUM_PORT},
-    "hubHost": "${SELENIUM_HOST}",
-    "hubPort": ${SELENIUM_PORT},
-    "maxSession": 1,
-    "register": true,
-    "registerCycle": 5000,
-    "cleanUpCycle": 5000,
-    "timeout": 150,
-    "browserTimeout": 0,
-    "nodeStatusCheckTimeout": 5000,
-    "nodePolling": 5000,
-    "role": "node",
-    "unregisterIfStillDownAfter": ${UNREGISTER_IF_STILL_DOWN_AFTER},
-    "downPollingLimit": 3,
-    "debug": false,
-    "servlets" : [],
-    "withoutServlets": [],
-    "custom": {}
-  }
+    {
+      "proxy": "com.zebrunner.mcloud.grid.MobileRemoteProxy",
+      "url":"http://${STF_PROVIDER_HOST}:${APPIUM_PORT}/wd/hub",
+      "host": "${STF_PROVIDER_HOST}",
+      "port": ${APPIUM_PORT},
+      "hubHost": "${SELENIUM_HOST}",
+      "hubPort": ${SELENIUM_PORT},
+      "maxSession": 1,
+      "register": true,
+      "registerCycle": 5000,
+      "cleanUpCycle": 5000,
+      "timeout": 150,
+      "browserTimeout": 0,
+      "nodeStatusCheckTimeout": 5000,
+      "nodePolling": 5000,
+      "role": "node",
+      "unregisterIfStillDownAfter": ${UNREGISTER_IF_STILL_DOWN_AFTER},
+      "downPollingLimit": 3,
+      "debug": false,
+      "servlets" : [],
+      "withoutServlets": [],
+      "custom": {}
+    }
 }
 EndOfMessage
