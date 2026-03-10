5d1fdd8c7d42231c60590ea5917ce41df2a1b32c
azarouski <azarouski@solvd.com>
2026-03-10 13:38:24 +0100

    zbr-* config/capabilities files were prettified

--- files/zbr-config-gen.sh
+++ files/zbr-config-gen.sh
@@ -2,47 +2,45 @@
 
 #IMPORTANT!!! Don't do any echo otherwise you corrupt generated nodeconfig.json
 # convert to lower case using Linux/Mac compatible syntax (bash v3.2)
-PLATFORM_NAME=`echo "$PLATFORM_NAME" |  tr '[:upper:]' '[:lower:]'`
+PLATFORM_NAME="$(printf '%s' "$PLATFORM_NAME" | tr '[:upper:]' '[:lower:]')"
 cat << EndOfMessage
 {
-  "capabilities":
-    [
-      {
-        "maxInstances": 1,
-        "deviceName": "${DEVICE_NAME}",
-        "deviceType": "${DEVICETYPE}",
-        "platformName":"${PLATFORM_NAME}",
-        "platformVersion":"${PLATFORM_VERSION}",
-        "udid": "${DEVICE_UDID}",
-        "adb_port": ${ADB_PORT},
-        "proxy_port": ${PROXY_PORT},
-        "automationName": "${AUTOMATION_NAME}",
-        "server_proxy_port": ${SERVER_PROXY_PORT}
-      }
-    ],
-  "configuration":
+  "capabilities": [
     {
-      "proxy": "com.zebrunner.mcloud.grid.MobileRemoteProxy",
-      "url":"http://${STF_PROVIDER_HOST}:${APPIUM_PORT}/wd/hub",
-      "host": "${STF_PROVIDER_HOST}",
-      "port": ${APPIUM_PORT},
-      "hubHost": "${SELENIUM_HOST}",
-      "hubPort": ${SELENIUM_PORT},
-      "maxSession": 1,
-      "register": true,
-      "registerCycle": 5000,
-      "cleanUpCycle": 5000,
-      "timeout": 150,
-      "browserTimeout": 0,
-      "nodeStatusCheckTimeout": 5000,
-      "nodePolling": 5000,
-      "role": "node",
-      "unregisterIfStillDownAfter": ${UNREGISTER_IF_STILL_DOWN_AFTER},
-      "downPollingLimit": 3,
-      "debug": false,
-      "servlets" : [],
-      "withoutServlets": [],
-      "custom": {}
+      "maxInstances": 1,
+      "deviceName": "${DEVICE_NAME}",
+      "deviceType": "${DEVICETYPE}",
+      "platformName":"${PLATFORM_NAME}",
+      "platformVersion":"${PLATFORM_VERSION}",
+      "udid": "${DEVICE_UDID}",
+      "adb_port": ${ADB_PORT},
+      "proxy_port": ${PROXY_PORT},
+      "automationName": "${AUTOMATION_NAME}",
+      "server_proxy_port": ${SERVER_PROXY_PORT}
     }
+  ],
+  "configuration": {
+    "proxy": "com.zebrunner.mcloud.grid.MobileRemoteProxy",
+    "url":"http://${STF_PROVIDER_HOST}:${APPIUM_PORT}/wd/hub",
+    "host": "${STF_PROVIDER_HOST}",
+    "port": ${APPIUM_PORT},
+    "hubHost": "${SELENIUM_HOST}",
+    "hubPort": ${SELENIUM_PORT},
+    "maxSession": 1,
+    "register": true,
+    "registerCycle": 5000,
+    "cleanUpCycle": 5000,
+    "timeout": 150,
+    "browserTimeout": 0,
+    "nodeStatusCheckTimeout": 5000,
+    "nodePolling": 5000,
+    "role": "node",
+    "unregisterIfStillDownAfter": ${UNREGISTER_IF_STILL_DOWN_AFTER},
+    "downPollingLimit": 3,
+    "debug": false,
+    "servlets" : [],
+    "withoutServlets": [],
+    "custom": {}
+  }
 }
 EndOfMessage
--- files/zbr-default-caps-gen.sh
+++ files/zbr-default-caps-gen.sh
@@ -3,18 +3,18 @@
 #IMPORTANT!!! Don't do any echo otherwise you corrupt generated defaultcapabilities json
 
 # convert to lower case using Linux/Mac compatible syntax (bash v3.2)
-PLATFORM_NAME=`echo "$PLATFORM_NAME" |  tr '[:upper:]' '[:lower:]'`
+PLATFORM_NAME="$(printf '%s' "$PLATFORM_NAME" | tr '[:upper:]' '[:lower:]')"
 if [[ "${PLATFORM_NAME}" == "android" ]]; then
 cat << EndOfMessage
 {
-  "platformName":"${PLATFORM_NAME}",
-  "appium:platformVersion":"${PLATFORM_VERSION}",
-  "appium:deviceName": "${DEVICE_NAME}",
-  "appium:udid": "${DEVICE_UDID}",
-  "appium:automationName": "${AUTOMATION_NAME}",
-  "appium:remoteAdbHost": "${REMOTE_ADB_HOST}",
-  "appium:chromedriverPort": ${CHROMEDRIVER_PORT},
-  "appium:chromeOptions": {${CHROME_OPTIONS}}
+ "platformName":"${PLATFORM_NAME}",
+ "appium:platformVersion":"${PLATFORM_VERSION}",
+ "appium:deviceName": "${DEVICE_NAME}",
+ "appium:udid": "${DEVICE_UDID}",
+ "appium:automationName": "${AUTOMATION_NAME}",
+ "appium:remoteAdbHost": "${REMOTE_ADB_HOST}",
+ "appium:chromedriverPort": ${CHROMEDRIVER_PORT},
+ "appium:chromeOptions": {${CHROME_OPTIONS}}
 }
 EndOfMessage
 fi
@@ -26,17 +26,17 @@ fi
 if [[ "${PLATFORM_NAME}" == "ios" ]]; then
 cat << EndOfMessage
 {
-  "platformName": "${PLATFORM_NAME}",
-  "appium:platformVersion": "${PLATFORM_VERSION}",
-  "appium:deviceName": "${DEVICE_NAME}",
-  "appium:udid":"$DEVICE_UDID",
-  "appium:automationName":"${AUTOMATION_NAME}",
-  "appium:mjpegServerPort": ${MJPEG_PORT},
-  "appium:clearSystemFiles": "false",
-  "appium:webDriverAgentUrl":"http://${WDA_HOST}:${WDA_PORT}",
-  "appium:preventWDAAttachments": "true",
-  "appium:simpleIsVisibleCheck": "true",
-  "appium:wdaLocalPort": "${WDA_PORT}"
+ "platformName": "${PLATFORM_NAME}",
+ "appium:platformVersion": "${PLATFORM_VERSION}",
+ "appium:deviceName": "${DEVICE_NAME}",
+ "appium:udid":"$DEVICE_UDID",
+ "appium:automationName":"${AUTOMATION_NAME}",
+ "appium:mjpegServerPort": ${MJPEG_PORT},
+ "appium:clearSystemFiles": "false",
+ "appium:webDriverAgentUrl":"http://${WDA_HOST}:${WDA_PORT}",
+ "appium:preventWDAAttachments": "true",
+ "appium:simpleIsVisibleCheck": "true",
+ "appium:wdaLocalPort": "${WDA_PORT}"
 }
 EndOfMessage
 fi
